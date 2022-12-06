a5825cc082a6bad9fc6739ba918452fbe285a19d
gjmzj <jmgaozz@hotmail.com>
2022-12-06 15:01:41 +0800

    modify download directory

--- multi-platform-download.sh
+++ multi-platform-download.sh
@@ -15,21 +15,21 @@ case "$ARC" in
       ;;
 esac
 
-mkdir -p /cni /containerd
+mkdir -p /extra/{cni,containerd}
 
 wget "https://github.com/containernetworking/plugins/releases/download/${CNI_PLUGINS_VERSION}/cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" && \
-tar zxf "cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" -C /cni && \
+tar zxf "cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz" -C /extra/cni && \
 rm -rf "cni-plugins-linux-${ARCH}-${CNI_PLUGINS_VERSION}.tgz"
 
 wget "https://github.com/kubernetes-sigs/cri-tools/releases/download/${CRICTL_VERSION}/crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" && \
-tar zxf "crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" -C /containerd && \
+tar zxf "crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz" -C /extra/containerd && \
 rm -rf "crictl-${CRICTL_VERSION}-linux-${ARCH}.tar.gz"
 
 wget "https://github.com/containerd/containerd/releases/download/v${CONTAINERD_VER}/containerd-${CONTAINERD_VER}-linux-${ARCH}.tar.gz" && \
 tar zxf "containerd-${CONTAINERD_VER}-linux-${ARCH}.tar.gz" -C /tmp && \
-mv /tmp/bin/* /containerd && \
+mv /tmp/bin/* /extra/containerd && \
 rm -rf "containerd-${CONTAINERD_VER}-linux-${ARCH}.tar.gz"
 
 wget "https://github.com/opencontainers/runc/releases/download/${RUNC_VER}/runc.${ARCH}" && \
-mv runc.${ARCH} /containerd/runc && \
-chmod +x /containerd/runc
+mv runc.${ARCH} /extra/containerd/runc && \
+chmod +x /extra/containerd/runc
